e21bc3c07dae6522077a16121b232e724bb1f6c7
Adam Foerster <adam@hariken.co>
2018-08-02 18:43:43 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photo/photo.component';
+import { VerseComponent } from './verse/verse.component';
+import { MusicComponent } from './music/music.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent,
+    VerseComponent,
+    MusicComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: "AIzaSyDN9PKe08IDHnL5ea_ZIw9SQWPsGPot1xc",
+    authDomain: "de-um-tempo.firebaseapp.com",
+    databaseURL: "https://de-um-tempo.firebaseio.com",
+    projectId: "de-um-tempo",
+    storageBucket: "de-um-tempo.appspot.com",
+    messagingSenderId: "475759243077"
+  }
 };
